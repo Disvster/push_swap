01dc11a50b75d5a1a7039278d16b032e4398ac53
Manuel Maria Carreiro Couto <manmaria@c1r10s5.42porto.com>
2025-08-23 00:16:03 +0100

    added the new functions

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: manmaria <manmaria@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 20:09:11 by manmaria          #+#    #+#             */
-/*   Updated: 2025/08/11 20:41:57 by manmaria         ###   ########.fr       */
+/*   Updated: 2025/08/22 20:16:04 by manmaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,19 +22,31 @@ typedef struct s_stack
 {
 	int				value;
 	int				index;
+	int				chunkid;
 	struct s_stack	*next;
 }					t_stack;
+
+typedef struct s_chunk
+{
+	int	size;
+	int	count;
+}				t_chunk;
 
 /*  Array Related Functions  */
 int		ft_find_index(long *tab, int size, int nbr);
 int		ft_sort_ltab(long *tab, int size);
 long	*create_ltab(int ac, char **av);
+int		ft_find_index(long *tab, int size, int nbr);
+
+int		ft_sqrt_floor(int nb);
+
 
 /*  Stack Related Functions  */
 void	stack_add_front(t_stack **stack, t_stack *new_nd);
-t_stack	*stack_newnode(int value, int index);
+t_stack	*stack_newnode(int value, int index, int chunk_size);
 t_stack	*ft_stacklast(t_stack *lst);
 void	ft_stack_clear(t_stack *lst);
+t_chunk	ft_chunkinit(int stack_size);
 
 /*  Movements/Operations Functions  */
 t_stack	**ft_stack_swap(t_stack **top);
