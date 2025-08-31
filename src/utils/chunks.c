6af7fd2811b5b4e6005f1e27103f6853a7483e31
Disvster <mmcouto2001@gmail.com>
2025-08-31 15:41:53 +0100

    norm corrected ft_sqrt

--- src/utils/chunks.c
+++ src/utils/chunks.c
@@ -6,39 +6,11 @@
 /*   By: manmaria <manmaria@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 19:48:56 by manmaria          #+#    #+#             */
-/*   Updated: 2025/08/31 15:32:29 by manmaria         ###   ########.fr       */
+/*   Updated: 2025/08/31 15:41:19 by manmaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incs/push_swap.h"
-
-int ft_sqrt_floor(int nb)
-{
-    int low;
-    int high;
-    int mid;
-    int result;
-
-    if (nb <= 1)
-        return (nb == 1);
-    low = 1;
-    high = nb / 2;
-    result = 0;
-    while (low <= high)
-    {
-        mid = low + (high - low) / 2;
-        if (mid * mid == nb)
-            return (mid);
-        else if (mid * mid < nb)
-        {
-            result = mid;
-            low = mid + 1;
-        }
-        else
-            high = mid - 1;
-    }
-    return (result);
-}
 
 t_chunk	ft_chunkinit(int stack_size)
 {
@@ -49,21 +21,30 @@ t_chunk	ft_chunkinit(int stack_size)
 	return (chunki);
 }
 
-int ft_sqrt_floor(int nb)
+int	ft_sqrt(int nb)
 {
-    int low;
-    int high;
-    int mid;
-    int result;
+	int	low;
+	int	high;
+	int	mid;
+	int	result;
 
-    if (nb <= 1)
-        return (nb == 1);
-    low = 1;
-    high = nb / 2;
-    result = 0;
-    while (low <= high)
-    {
-        
-    }
-    return (result);
+	if (nb <= 1)
+		return (nb == 1);
+	low = 1;
+	high = nb / 2;
+	result = 0;
+	while (low <= high)
+	{
+		mid = low + (high - low) / 2;
+		if (mid * mid == nb)
+			return (mid);
+		else if (mid * mid < nb)
+		{
+			result = mid;
+			low = mid + 1;
+		}
+		else
+			high = mid - 1;
+	}
+	return (result);
 }
